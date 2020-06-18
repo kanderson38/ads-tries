f50234acacac88ed5b1ac1dce2e9ba3ca93cf73e
Kirsten Anderson <kirsten@kirstenmunson.com>
2020-06-17 17:30:32 -0700

    all tests pass

--- src/data_structures/test/prefix_dictionary.test.js
+++ src/data_structures/test/prefix_dictionary.test.js
@@ -1,4 +1,4 @@
-import dataStructures from '../data_structure_list';
+import dataStructures from "../data_structure_list";
 
 const identity = (word) => word;
 
@@ -6,37 +6,37 @@ const lowerCase = (word) => word.toLowerCase();
 
 const verifyLookupResults = (received, expected) => {
   expect(received.sort()).toStrictEqual(expected.sort());
-}
+};
 
-Object.values(dataStructures).forEach(PrefixDictionary => {
+Object.values(dataStructures).forEach((PrefixDictionary) => {
   describe(PrefixDictionary, () => {
-    describe('constructor', () => {
-      it('creates a trie with a count of 0 if no words are given', () => {
+    describe("constructor", () => {
+      it("creates a trie with a count of 0 if no words are given", () => {
         const pd = new PrefixDictionary([], identity);
         expect(pd.count()).toBe(0);
       });
 
-      it('counts unique words', () => {
+      it("counts unique words", () => {
         const pd = new PrefixDictionary(
-          ['ada', 'developers', 'academy'],
+          ["ada", "developers", "academy"],
           identity
         );
 
         expect(pd.count()).toBe(3);
       });
 
-      it('does not count duplicate words', () => {
+      it("does not count duplicate words", () => {
         const pd = new PrefixDictionary(
-          ['ada', 'developers', 'academy', 'academy'],
+          ["ada", "developers", "academy", "academy"],
           identity
         );
 
         expect(pd.count()).toBe(3);
       });
 
-      it('counts different words with the same code', () => {
+      it("counts different words with the same code", () => {
         const pd = new PrefixDictionary(
-          ['ada', 'developers', 'ADA', 'DeVeLoPeRs'],
+          ["ada", "developers", "ADA", "DeVeLoPeRs"],
           lowerCase
         );
 
@@ -44,239 +44,200 @@ Object.values(dataStructures).forEach(PrefixDictionary => {
       });
     });
 
-    describe('addWord', () => {
-      it('increases count by one for new words', () => {
+    describe("addWord", () => {
+      it("increases count by one for new words", () => {
         const pd = new PrefixDictionary(
-          ['ada', 'developers', 'academy'],
+          ["ada", "developers", "academy"],
           identity
         );
 
         expect(pd.count()).toBe(3);
 
-        ['advanced', 'data', 'structures'].forEach((word, i) => {
+        ["advanced", "data", "structures"].forEach((word, i) => {
           pd.addWord(word);
           expect(pd.count()).toBe(4 + i);
         });
       });
 
-      it('ignores words added at initialization', () => {
+      it("ignores words added at initialization", () => {
         const pd = new PrefixDictionary(
-          ['ada', 'developers', 'academy'],
+          ["ada", "developers", "academy"],
           identity
         );
 
         expect(pd.count()).toBe(3);
 
-        ['ada', 'developers', 'academy'].forEach((word, i) => {
+        ["ada", "developers", "academy"].forEach((word, i) => {
           pd.addWord(word);
           expect(pd.count()).toBe(3);
         });
       });
 
-      it('ignores words added after initialization', () => {
-        const pd = new PrefixDictionary(
-          [],
-          identity
-        );
+      it("ignores words added after initialization", () => {
+        const pd = new PrefixDictionary([], identity);
 
         expect(pd.count()).toBe(0);
 
-        ['ada', 'developers', 'academy'].forEach((word, i) => {
+        ["ada", "developers", "academy"].forEach((word, i) => {
           pd.addWord(word);
           expect(pd.count()).toBe(1 + i);
         });
 
-        ['ada', 'developers', 'academy'].forEach((word, i) => {
+        ["ada", "developers", "academy"].forEach((word, i) => {
           pd.addWord(word);
           expect(pd.count()).toBe(3);
         });
       });
 
-      it('counts different words with the same code', () => {
+      it("counts different words with the same code", () => {
         const pd = new PrefixDictionary(
-          ['ada', 'developers', 'academy'],
+          ["ada", "developers", "academy"],
           identity
         );
 
         expect(pd.count()).toBe(3);
 
-        ['ADA', 'DeVeLoPeRs', 'Academy'].forEach((word, i) => {
+        ["ADA", "DeVeLoPeRs", "Academy"].forEach((word, i) => {
           pd.addWord(word);
           expect(pd.count()).toBe(4 + i);
         });
       });
     });
 
-    describe('lookupCode', () => {
-      it('finds a word matching the code', () => {
+    describe("lookupCode", () => {
+      it("finds a word matching the code", () => {
         const pd = new PrefixDictionary(
-          ['ada', 'developers', 'academy'],
+          ["ada", "developers", "academy"],
           identity
         );
 
-        verifyLookupResults(
-          pd.lookupCode('ada'),
-          ['ada']
-        );
+        verifyLookupResults(pd.lookupCode("ada"), ["ada"]);
       });
 
-      it('finds all words matching the code', () => {
+      it("finds all words matching the code", () => {
         const pd = new PrefixDictionary(
-          ['ada', 'developers', 'academy', 'ADA', 'AdA'],
+          ["ada", "developers", "academy", "ADA", "AdA"],
           lowerCase
         );
 
-        verifyLookupResults(
-          pd.lookupCode('ada'),
-          ['ada', 'ADA', 'AdA']
-        );
+        verifyLookupResults(pd.lookupCode("ada"), ["ada", "ADA", "AdA"]);
       });
 
-      it('finds words added after initialization', () => {
+      it("finds words added after initialization", () => {
         const pd = new PrefixDictionary(
-          ['ada', 'developers', 'academy'],
+          ["ada", "developers", "academy"],
           lowerCase
         );
 
-        pd.addWord('ADA');
-        pd.addWord('AdA');
+        pd.addWord("ADA");
+        pd.addWord("AdA");
 
-        verifyLookupResults(
-          pd.lookupCode('ada'),
-          ['ada', 'ADA', 'AdA']
-        );
+        verifyLookupResults(pd.lookupCode("ada"), ["ada", "ADA", "AdA"]);
       });
 
-      it('finds words added along an existing path', () => {
+      it("finds words added along an existing path", () => {
         const pd = new PrefixDictionary(
-          ['adamant', 'developers', 'academy'],
+          ["adamant", "developers", "academy"],
           identity
         );
 
-        pd.addWord('ada');
+        pd.addWord("ada");
 
-        verifyLookupResults(
-          pd.lookupCode('ada'),
-          ['ada']
-        );
+        verifyLookupResults(pd.lookupCode("ada"), ["ada"]);
       });
 
-      it('returns an empty array if no words match', () => {
+      it("returns an empty array if no words match", () => {
         const pd = new PrefixDictionary(
-          ['ada', 'developers', 'academy'],
+          ["ada", "developers", "academy"],
           identity
         );
 
-        verifyLookupResults(
-          pd.lookupCode('nope'),
-          []
-        );
+        verifyLookupResults(pd.lookupCode("nope"), []);
       });
 
-      it('ignores words where the code prefix matches', () => {
+      it("ignores words where the code prefix matches", () => {
         const pd = new PrefixDictionary(
-          ['ada', 'developers', 'academy'],
+          ["ada", "developers", "academy"],
           identity
         );
 
-        pd.addWord('adamant');
+        pd.addWord("adamant");
 
-        verifyLookupResults(
-          pd.lookupCode('ada'),
-          ['ada']
-        );
+        verifyLookupResults(pd.lookupCode("ada"), ["ada"]);
       });
     });
 
-    describe('lookupPrefix', () => {
-      it('finds a word matching the prefix entirely', () => {
+    describe("lookupPrefix", () => {
+      it("finds a word matching the prefix entirely", () => {
         const pd = new PrefixDictionary(
-          ['ada', 'developers', 'academy'],
+          ["ada", "developers", "academy"],
           identity
         );
 
-        verifyLookupResults(
-          pd.lookupPrefix('ada'),
-          ['ada']
-        );
+        verifyLookupResults(pd.lookupPrefix("ada"), ["ada"]);
       });
 
-      it('finds a word where the code starts with the prefix', () => {
+      it("finds a word where the code starts with the prefix", () => {
         const pd = new PrefixDictionary(
-          ['adamant', 'developers', 'academy'],
+          ["adamant", "developers", "academy"],
           identity
         );
 
-        verifyLookupResults(
-          pd.lookupPrefix('ada'),
-          ['adamant']
-        );
+        verifyLookupResults(pd.lookupPrefix("ada"), ["adamant"]);
       });
 
-      it('finds all words matching the prefix', () => {
+      it("finds all words matching the prefix", () => {
         const pd = new PrefixDictionary(
-          ['ada', 'developers', 'academy', 'ADA', 'AdA', 'adamant', 'AdAmAnT'],
+          ["ada", "developers", "academy", "ADA", "AdA", "adamant", "AdAmAnT"],
           lowerCase
         );
 
-        verifyLookupResults(
-          pd.lookupPrefix('ada'),
-          ['ada', 'ADA', 'AdA', 'adamant', 'AdAmAnT']
-        );
+        verifyLookupResults(pd.lookupPrefix("ada"), [
+          "ada",
+          "ADA",
+          "AdA",
+          "adamant",
+          "AdAmAnT",
+        ]);
       });
 
-      it('finds words added after initialization', () => {
+      it("finds words added after initialization", () => {
         const pd = new PrefixDictionary(
-          ['ada', 'developers', 'academy'],
+          ["ada", "developers", "academy"],
           lowerCase
         );
 
-        pd.addWord('ADA');
-        pd.addWord('adamant');
+        pd.addWord("ADA");
+        pd.addWord("adamant");
 
-        verifyLookupResults(
-          pd.lookupPrefix('ada'),
-          ['ada', 'ADA', 'adamant']
-        );
+        verifyLookupResults(pd.lookupPrefix("ada"), ["ada", "ADA", "adamant"]);
       });
 
-      it('finds words added along an existing path', () => {
+      it("finds words added along an existing path", () => {
         const pd = new PrefixDictionary(
-          ['adamant', 'developers', 'academy'],
+          ["adamant", "developers", "academy"],
           identity
         );
 
-        pd.addWord('ada');
+        pd.addWord("ada");
 
-        verifyLookupResults(
-          pd.lookupPrefix('ada'),
-          ['ada', 'adamant']
-        );
+        verifyLookupResults(pd.lookupPrefix("ada"), ["ada", "adamant"]);
       });
 
-      it('returns an empty array if no words match', () => {
+      it("returns an empty array if no words match", () => {
         const pd = new PrefixDictionary(
-          ['ada', 'developers', 'academy'],
+          ["ada", "developers", "academy"],
           identity
         );
 
-        verifyLookupResults(
-          pd.lookupPrefix('nope'),
-          []
-        );
+        verifyLookupResults(pd.lookupPrefix("nope"), []);
       });
 
-      it('ignores words where the code is the prefix of the prefix', () => {
-        const pd = new PrefixDictionary(
-          ['ada', 'a', 'ad'],
-          identity
-        );
+      it("ignores words where the code is the prefix of the prefix", () => {
+        const pd = new PrefixDictionary(["ada", "a", "ad"], identity);
 
-        verifyLookupResults(
-          pd.lookupPrefix('ada'),
-          ['ada']
-        );
-      })
+        verifyLookupResults(pd.lookupPrefix("ada"), ["ada"]);
+      });
     });
   });
-}) 
+});
--- src/data_structures/trie.js
+++ src/data_structures/trie.js
@@ -6,22 +6,70 @@ class TrieNode {
 }
 
 class Trie {
-  constructor(words, buildCode, Node=TrieNode) {
+  constructor(words, buildCode, Node = TrieNode) {
     this.Node = Node;
     this.buildCode = buildCode;
     this._root = new this.Node();
     this._count = 0;
-    words.forEach(word => this.addWord(word));
+    words.forEach((word) => this.addWord(word));
   }
 
   addWord(word) {
-    const code = this.buildCode(word);
+    const code = this.buildCode(word).split("");
+    let node = this._root;
+    code.forEach((char) => {
+      if (!node.children[char]) {
+        node.children[char] = new TrieNode();
+      }
+      node = node.children[char];
+    });
+    if (node.words.indexOf(word) === -1) {
+      node.words.push(word);
+      this._count += 1;
+    }
   }
 
   lookupCode(code) {
+    let node = this._root;
+    const splitCode = code.split("");
+    for (let char of splitCode) {
+      if (!node.children[char]) {
+        return [];
+      }
+      node = node.children[char];
+    }
+    return node.words;
+  }
+
+  traverseSubtries(node, results) {
+    node.words.forEach((word) => {
+      results.push(word);
+    });
+    if (!Object.keys(node.children).length) {
+      return;
+    }
+    Object.keys(node.children).forEach((key) => {
+      this.traverseSubtries(node.children[key], results);
+    });
   }
 
   lookupPrefix(codePrefix) {
+    let node = this._root;
+
+    let results = [];
+    const splitCode = codePrefix.split("");
+    for (let char of splitCode) {
+      if (!node.children[char]) {
+        return results;
+      }
+      node = node.children[char];
+    }
+    if (Object.keys(node.children).length) {
+      this.traverseSubtries(node, results);
+    } else {
+      results = node.words;
+    }
+    return results;
   }
 
   count() {
